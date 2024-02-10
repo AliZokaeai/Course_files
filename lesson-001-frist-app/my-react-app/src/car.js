ff34a1a30d67387dacc599317adacb5096d300cc
AliZokaeai <alizokaeaidehaghani@gmail.com>
2024-02-10 12:06:24 +0330

    learning mor about react until Forms

--- lesson-001-frist-app/my-react-app/src/car.js
+++ lesson-001-frist-app/my-react-app/src/car.js
@@ -1,5 +1,32 @@
-function Car(props){
-    return <h2>Hi, I am a {props.color} car!</h2>
+import React from 'react';
+class Car extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      brand:"ford",
+      model:"Mustang",
+      color:"red",
+      year:1964
+    };
   }
+  changeColor =() =>{
+    this.setState ({color:"blue"});
+  }
+  render() {
+    return (
+       <div>
+        <h1>My {this.state.brand}</h1>
+        <p>
+          It is a {this.state.color} {this.state.model} from {this.state.year}.
+        </p>
+        <button
+        type ="button"
+        onClick={this.changeColor}>
+          changeColor
+        </button>
+       </div>
+    );
+  }
+}
 
-  export default Car;
+export default Car;
--- lesson-001-frist-app/my-react-app/src/index.js
+++ lesson-001-frist-app/my-react-app/src/index.js
@@ -1,17 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Car from './car.js';
 
-function Grage(props){
-  return(
-    <>
-      <h1>Who live in my Grage?</h1>
-      <Car color ="green"/>
-    </>
-  )
+
+function Car(props){
+    return <li>I am a {props.keys} = {props.brand}</li>;
 }
 
+function Garage(){
+  const cars =[
+    {id:1, brand:'Ford'},
+    {id:2, brand:'BMW'},
+    {id:3, brand:'Audi'}
+  ];
 
+  return(
+
+    <>
+    <h1>Who lives in my Garage?</h1>
+    <ul>
+      {cars.map((car)=><Car keys={car.id} brand={car.brand}/>)}
+    </ul>
+    </>
+  );
+}
 const container = document.getElementById('root')
 const root = ReactDOM.createRoot(container);
-root.render(<Grage/>);
+root.render(<Garage/>);
